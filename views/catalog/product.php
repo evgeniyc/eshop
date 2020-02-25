dbe06d3ae6d38eb39630dc5217f373cf5fe0d6d9
evgeniyc <evgeniychp@i.ua>
2020-02-25 16:57:19 +0200

    tuesday work 25.02

--- views/catalog/product.php
+++ views/catalog/product.php
@@ -8,118 +8,108 @@ use app\components\BrandsWidget;
 use app\components\ChainWidget;
 use yii\helpers\Url;
 use yii\helpers\Html;
+use rmrevin\yii\fontawesome\FAS;
+
+$this->registerJs('function Quant(el){
+					var obj = $("#quant");
+					var value = obj.val();
+					var cls = $(el).attr("id");
+					switch(cls){
+						case "pquant": value++;
+						break;
+						case "mquant": value--;
+						break;
+					}
+					if(value < 1 ) value = 1;
+					obj.val(value);
+					}',3,'quant'); 
 ?>
 
 <section>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-3 left-sidebar">
-                <h2>Каталог</h2>
-                <div class="category-products">
-                    <?= TreeWidget::widget(); ?>
-                </div>
+	<?php //ChainWidget::widget(['itemCurrent' => $category['id'], 'showCurrent' => true]); ?>
+        <div class="row no-gutters row-flex">
+            <div class="col-sm-3">
+                <div id="left-sidebar">
+                    <h2>Каталог</h2>
+                    <div class="category-products">
+                        <?= TreeWidget::widget(); ?>
+                    </div>
 
-                <h2>Бренды</h2>
-                <div class="brand-products">
-                    <?= BrandsWidget::widget(); ?>
+                    <h2>Бренды</h2>
+                    <div class="brand-products">
+                        <?= BrandsWidget::widget(); ?>
+                    </div>
                 </div>
             </div>
 
             <div class="col-sm-9">
-                <?= ChainWidget::widget(['itemCurrent' => $product['category_id']]); ?>
-                <h1><?= Html::encode($product['name']); ?></h1>
-                <div class="row">
-                    <div class="col-sm-5">
-                        <div class="product-image">
-                            <?=
-                            Html::img(
-                                '@web/images/products/large/'.$product['image'],
-                                ['alt' => $product['name']]
-                            );
-                            ?>
-                        </div>
-                    </div>
-                    <div class="col-sm-7">
-                        <div class="product-info">
-                            <p class="product-price">
-                                Цена: <span><?= $product['price']; ?></span> руб.
-                            </p>
-                            <form method="post"
-                                  action="<?= Url::to(['basket/add']); ?>"
-                                  class="add-to-basket">
-                                <label>Количество</label>
-                                <input name="count" type="text" value="1" />
-                                <input type="hidden" name="id"
-                                       value="<?= $product['id']; ?>">
-                                <?=
-                                Html::hiddenInput(
-                                    Yii::$app->request->csrfParam,
-                                    Yii::$app->request->csrfToken
-                                );
-                                ?>
-                                <button type="submit"
-                                        class="btn btn-warning">
-                                    <i class="fa fa-shopping-cart"></i>
-                                    Добавить в корзину
-                                </button>
-                            </form>
-                            <p>Артикул: 1234567</p>
-                            <p>Наличие: На складе</p>
-                            <p>
-                                Бренд:
-                                <a href="<?= Url::to(['catalog/brand', 'id' => $brand['id']]); ?>">
-                                    <?= Html::encode($brand['name']); ?>
-                                </a>
-                            </p>
+				<div id="main-part">
+					<h1><?= Html::encode($product['name']); ?></h1>
+					<div class="row">
+						<div class="col-sm-5">
+							<div class="product-image">
+								<?=
+								Html::img(
+									'@web/images/products/large/'.$product['image'],
+									['alt' => $product['name']]
+								);
+								?>
+							</div>
+						</div>
+						<div class="col-sm-7">
+							<div class="prod-info">
+								<p class="product-price">
+									Цена: <span><?= $product['price']; ?></span> руб.
+								</p>
+								<form method="post"
+									  action="<?= Url::to(['basket/add']); ?>"
+									  class="add-to-basket">
+									<label>Количество</label>
+									<?= Html::button(FAS::i('plus'),[
+										'id' => 'pquant',
+										'onclick' => 'Quant(this)',
+										]
+									)?>
+									<input name="count" type="text" value="1" id="quant" />
+									<?= Html::button(FAS::i('minus'),[
+										'id' => 'mquant',
+										'onclick' => 'Quant(this)',
+										]
+									)?>
+									<input type="hidden" name="id"
+										   value="<?= $product['id']; ?>">
+									<?=
+									Html::hiddenInput(
+										Yii::$app->request->csrfParam,
+										Yii::$app->request->csrfToken
+									);
+									?>
+									<button type="submit"
+											class="btn btn-warning">
+										<i class="fa fa-shopping-cart"></i>
+										Добавить в корзину
+									</button>
+								</form>
+								<p>Артикул: 1234567</p>
+								<p>Наличие: На складе</p>
+								<p>
+									Бренд:
+									<a href="<?= Url::to(['catalog/brand', 'id' => $brand['id']]); ?>">
+										<?= Html::encode($brand['name']); ?>
+									</a>
+								</p>
 
-                        </div>
-                    </div>
-                </div>
-                <div class="product-descr">
-                    <?= $product['content']; ?>
-                </div>
-                <?php if (!empty($similar)): /* популярные товары */ ?>
-                    <h2>Популярные товары</h2>
-                    <div class="row">
-                        <?php foreach ($similar as $item): ?>
-                            <div class="col-sm-4">
-                                <div class="product-wrapper text-center">
-                                    <?=
-                                    Html::img(
-                                        '@web/images/products/medium/'.$item['image'],
-                                        ['alt' => $item['name'], 'class' => 'img-responsive']
-                                    );
-                                    ?>
-                                    <h2><?= $item['price']; ?> руб.</h2>
-                                    <p>
-                                        <a href="<?= Url::to(['catalog/product', 'id' => $item['id']]); ?>">
-                                            <?= Html::encode($item['name']); ?>
-                                        </a>
-                                    </p>
-                                    <a href="#" class="btn btn-warning">
-                                        <i class="fa fa-shopping-cart"></i>
-                                        Добавить в корзину
-                                    </a>
-                                    <?php
-                                    if ($item['new']) { // новинка?
-                                        echo Html::img(
-                                            '@web/images/home/new.png',
-                                            ['alt' => 'Новинка', 'class' => 'new']
-                                        );
-                                    }
-                                    if ($item['sale']) { // распродажа?
-                                        echo Html::img(
-                                            '@web/images/home/sale.png',
-                                            ['alt' => 'Распродажа', 'class' => 'sale']
-                                        );
-                                    }
-                                    ?>
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class="product-descr">
+						<?= $product['content']; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
+<div id="zero" class="row"></div>
